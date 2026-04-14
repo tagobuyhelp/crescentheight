27b918a17a1d490a806870b40f174cf769713f68
Tagobuy <tagobuyhelp@gmail.com>
2026-04-14 16:05:16 +0530

    ...

--- Client/eslint.config.mjs
+++ Client/eslint.config.mjs
@@ -1,7 +1,13 @@
-import nextCoreWebVitals from "eslint-config-next/core-web-vitals";
+import { FlatCompat } from "@eslint/eslintrc";
+import path from "path";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const compat = new FlatCompat({ baseDirectory: __dirname });
 
 const eslintConfig = [
-  ...nextCoreWebVitals,
+  ...compat.extends("next/core-web-vitals"),
   {
     ignores: [
       "node_modules/**",
